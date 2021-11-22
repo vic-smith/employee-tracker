e1a50e8b4c2872136c35c538739f26d839a413a2
Victor Smith <victorj500@gmail.com>
2021-11-21 20:09:45 -0600

    added departments to view all employees

--- index.js
+++ index.js
@@ -45,9 +45,12 @@ const task = () => {
         );
       } else if (res.action === "view all employees") {
         db.query(
-          `SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary,  employees.manager_id AS manager
-          FROM roles
-          LEFT JOIN employees ON employees.role_id=roles.id;`,
+          `SELECT employees.id, employees.first_name, employees.last_name, roles.title, department.name AS department, roles.salary, employees.manager_id AS manager 
+          FROM employees
+          LEFT JOIN roles 
+          ON employees.role_id=roles.id
+          LEFT JOIN department
+          ON roles.department_id = department.id;`,
           (err, rows) => {
             if (err) throw err;
             console.table(rows);
